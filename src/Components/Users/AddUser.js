fa360d43c538ac03d99480c3a6a4b5c0575f4b9c
makoma mabilo <makoma44@gmail.com>
2022-12-09 12:16:47 +0800

    managed the error state, added wrapper component

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,18 +4,29 @@ import Card from '../UI/Card';
 import Button from '../UI/Button';
 import classes from './AddUser.module.css';
 import ErrorModal from '../UI/ErrorModal';
+import Wrapper from '../../Helpers/Wrapper';
 
 const AddUser = props => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error,setError] =useState('');
 
   const addUserHandler = (event) => {
    event.preventDefault();
    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+   setError({
+     title:'Invalid Input',
+     message:'Please enter a valid name and age(non empty values)'
+   });
+
     return;
   }
 
    if (enteredAge < 1 ) {
+    setError({
+      title:'Invalid age',
+      message:'Please enter a valid age (> 0.'
+    });
     return;
   }
 
@@ -33,9 +44,19 @@ const AddUser = props => {
    setEnteredAge(event.target.value)
  };
 
+ const errorHandler = () => {
+  setError(null);
+ };
+
  return (
-  <div>
-  <ErrorModal title="An Error Occured" message="Something went wrong!">
+  <Wrapper>
+    {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
    <Card className={classes.input}>
     <form onSubmit={addUserHandler}>
      <label htmlFor="username">Username</label>
@@ -45,9 +66,8 @@ const AddUser = props => {
      <Button type="submit">Add User</Button>
     </form>
    </Card>
-  </ErrorModal>
-  </div>
- );
+   </Wrapper>
+  );
 };
 
 export default AddUser;
